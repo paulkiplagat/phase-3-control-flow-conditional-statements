730b03f0f97d930e16bd31d2016dc56505706e32
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-15 16:27:37 +0300

    another commit

--- control_flow.rb
+++ control_flow.rb
@@ -18,3 +18,14 @@ def hows_the_weather(temperature)
 
   "It's #{response} out there!"
 end
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
+end

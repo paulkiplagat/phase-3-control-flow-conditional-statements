18b977d5cfd09bef42ab878abe0cc8be0534d60c
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-15 16:26:19 +0300

    second commit

--- control_flow.rb
+++ control_flow.rb
@@ -5,3 +5,16 @@ def admin_login(username, password)
     "Access denied"
   end
 end
+def hows_the_weather(temperature)
+  response =  if temperature < 40
+                "brisk"
+              elsif temperature >= 40 && temperature <= 65
+                "a little chilly"
+              elsif temperature > 85
+                "too dang hot"
+              else
+                "perfect"
+              end
+
+  "It's #{response} out there!"
+end
